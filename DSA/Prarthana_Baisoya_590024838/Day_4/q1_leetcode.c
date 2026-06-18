7f94f03221891a3feee764e309035a02c85c8050
Prarthana Baisoya <baisoyaprarthana19@gmail.com>
2026-06-18 18:49:37 +0530

    Create q1_leetcode.c

---
+++ DSA/Prarthana_Baisoya_590024838/Day_4/q1_leetcode.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+
+    scanf("%d %d", &rows, &cols);
+
+    int a[100][100];
+    int transpose[100][100];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    for(int i = 0; i < cols; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
