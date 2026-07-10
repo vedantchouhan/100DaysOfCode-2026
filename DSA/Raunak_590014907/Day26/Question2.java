446892e47ef89d01992ef098efe51602772f4e7f
raunak-goyal <ugccrpo@gmail.com>
2026-07-10 15:42:13 +0530

    Day 26 solutions

---
+++ DSA/Raunak_590014907/Day26/Question2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public boolean tripletSumCheck(int[] arr) {
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 2; i--) {
+
+            int left = 0;
+            int right = i - 1;
+
+            while (left < right) {
+
+                int sum = arr[left] + arr[right];
+
+                if (sum == arr[i]) {
+                    return true;
+                } else if (sum < arr[i]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return false;
+    }
+}
